e02852c235327695c68fc75b3e4519f7c28cb317
Sachin Ninganure <sninganu@sninganu.pnq.csb>
2023-01-31 20:24:17 +0530

    run.sh changed

--- egress_2p.sh
+++ egress_2p.sh
@@ -44,7 +44,7 @@ echo "print ing the env variable from pipeline"
 echo $ENV_VARS;
 for i in {1..2}; do echo pod$i=mypod;mypod=$(oc get pods -n test$i|awk 'NR==2{print $1}');echo $mypod;echo $test$i;oc project test$i;egress=$(oc exec $mypod -- curl "$1":9095);echo $egress;done
 
-for i in {3..4}; do echo pod$i=mypod;mypod=$(oc get pods -n test$i|awk 'NR==2{print $1}');echo $mypod;echo $test$i;oc project test$i;egress=$(oc exec $mypod -- curl "$1":9095);echo $egress;done
+for i in {3..4}; do echo pod$i=mypod;mypod=$(oc get pods -n test$i|awk 'NR==2{print $1}');echo $mypod;echo $test$i;oc project test$i;egress=$(oc exec $mypod -- curl "$private_ip_address":9095);echo $egress;done
 #cluster is enabled with ipecho service and https://mastern-jenkins-csb-openshift-qe.apps.ocp-c1.prod.psi.redhat.com/job/ocp-common/job/ginkgo-test/ is run successfully 
 
 #Delete all the projects for next iteration
