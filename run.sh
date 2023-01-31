c8f7f5080afe8a84dfbaa008de0813748bbbd376
Sachin Ninganure <sninganu@sninganu.pnq.csb>
2023-01-31 22:08:29 +0530

    added ip_p

--- run.sh
+++ run.sh
@@ -1,16 +1,16 @@
 echo "print the env var"
-echo "$ENV_VARS"
+echo "ip_p"
 echo -e "\033[1m$(date -u) ${@}\033[0m" >>result01.txt
-./egress_2p.sh $ENV_VARS>>result01.txt;sleep 5
+./egress_2p.sh ip_p>>result01.txt;sleep 5
 
 echo -e "\033[1m$(date -u) ${@}\033[0m" >>result02.txt
-./egress_2p.sh $ENV_VARS>>result02.txt;sleep 5
+./egress_2p.sh ip_p>>result02.txt;sleep 5
 
 echo -e "\033[1m$(date -u) ${@}\033[0m" >>result03.txt
-./egress_2p.sh $ENV_VARS>>result03.txt;sleep 5
+./egress_2p.sh ip_p>>result03.txt;sleep 5
 
 echo -e "\033[1m$(date -u) ${@}\033[0m" >>result04.txt
-./egress_2p.sh $ENV_VARS>>result04.txt
+./egress_2p.sh ip_p>>result04.txt
 
 echo -e "\033[1m$(date -u) ${@}\033[0m" >>result05.txt
-./egress_2p.sh $ENV_VARS>>result05.txt
+./egress_2p.sh ip_p>>result05.txt
