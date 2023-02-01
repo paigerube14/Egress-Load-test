6f632996ae812f8b7cc449b6671244b4ca8f90fd
Sachin Ninganure <sninganu@sninganu.pnq.csb>
2023-02-01 11:49:47 +0530

    added $ip

--- run.sh
+++ run.sh
@@ -1,16 +1,17 @@
 echo "print the env var"
 echo "private_ip_address"
+echo "$private_ip_address"
 echo -e "\033[1m$(date -u) ${@}\033[0m" >>result01.txt
-./egress_2p.sh private_ip_address>>result01.txt;sleep 5
+./egress_2p.sh $private_ip_address>>result01.txt;sleep 5
 
 echo -e "\033[1m$(date -u) ${@}\033[0m" >>result02.txt
-./egress_2p.sh private_ip_address>>result02.txt;sleep 5
+./egress_2p.sh $private_ip_address>>result02.txt;sleep 5
 
 echo -e "\033[1m$(date -u) ${@}\033[0m" >>result03.txt
-./egress_2p.sh private_ip_address>>result03.txt;sleep 5
+./egress_2p.sh $private_ip_address>>result03.txt;sleep 5
 
 echo -e "\033[1m$(date -u) ${@}\033[0m" >>result04.txt
-./egress_2p.sh private_ip_address>>result04.txt
+./egress_2p.sh $private_ip_address>>result04.txt
 
 echo -e "\033[1m$(date -u) ${@}\033[0m" >>result05.txt
-./egress_2p.sh private_ip_address>>result05.txt
+./egress_2p.sh $private_ip_address>>result05.txt
